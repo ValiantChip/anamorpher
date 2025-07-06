173a79ffb01c0617d323d105f90361cf40568787
ValiantChip <egb154@gmail.com>
2025-07-05 22:40:14 -0400

    changes to commandline args

--- anamorph.go
+++ anamorph.go
@@ -19,41 +19,55 @@ func ReturnWithCode() int {
 	var outheight int
 	var outwidth int
 	var scale float64
+	var help bool
 
-	flag.IntVar(&radius, "r", 0, "radius")
+	flag.CommandLine.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "anamorph [input path] [options]\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Options:\n")
+		flag.PrintDefaults()
+	}
+
+	flag.IntVar(&radius, "r", 100, "radius")
 	flag.IntVar(&degrees, "d", 45, "degrees")
-	flag.StringVar(&inputPath, "i", "", "input path")
 	flag.StringVar(&outputpath, "o", "./out.jpg", "output path")
-	flag.IntVar(&outheight, "h", 0, "output height")
+	flag.IntVar(&outheight, "height", 0, "output height")
 	flag.IntVar(&outwidth, "w", 0, "output width")
 	flag.Float64Var(&scale, "s", 1.0, "scale")
+	flag.BoolVar(&help, "h", false, "\nshow this message")
 	flag.Parse()
 
-	if radius < 1 {
-		fmt.Printf("radius must be set to a value greater than 0\n")
+	if help {
+		flag.CommandLine.Usage()
+		return 0
+	}
+
+	inputPath = flag.Arg(0)
+	if inputPath == "" {
+		fmt.Printf("Error: input path must be set\n")
 		return 2
 	}
 
-	if inputPath == "" {
-		fmt.Printf("input path must be set\n")
+	if radius < 1 {
+		fmt.Printf("Error: radius must be set to a value greater than 0\n")
 		return 2
 	}
 
 	if scale < 1.0 {
-		fmt.Printf("scale must not be less than 1\n")
+		fmt.Printf("Error: scale must not be less than 1\n")
 		return 2
 	}
 
 	fl, err := os.Open(inputPath)
 	if err != nil {
-		fmt.Printf("invalid input path\n")
+		fmt.Printf("Error: invalid input path\n")
 		return 2
 	}
 	defer fl.Close()
 
 	out, err := os.Create(outputpath)
 	if err != nil {
-		fmt.Printf("error creating output file\n")
+		fmt.Printf("Error: error creating output file\n")
 		return 1
 	}
 
@@ -61,7 +75,7 @@ func ReturnWithCode() int {
 
 	img, err := jpeg.Decode(fl)
 	if err != nil {
-		fmt.Printf("error decoding image: %s\n", err.Error())
+		fmt.Printf("Error: error decoding image: %s\n", err.Error())
 		return 1
 	}
 
@@ -82,13 +96,13 @@ func ReturnWithCode() int {
 
 	err = morph.Anamorph()
 	if err != nil {
-		fmt.Printf("error anamorphing image: %s\n", err.Error())
+		fmt.Printf("Error: error anamorphing image: %s\n", err.Error())
 		return 1
 	}
 
 	err = jpeg.Encode(out, morph.Mod, nil)
 	if err != nil {
-		fmt.Printf("error encoding image: %s\n", err.Error())
+		fmt.Printf("Error: error encoding image: %s\n", err.Error())
 		return 1
 	}
 
@@ -97,7 +111,12 @@ func ReturnWithCode() int {
 }
 
 func main() {
-	os.Exit(ReturnWithCode())
+	code := ReturnWithCode()
+	if code == 2 {
+		flag.CommandLine.Usage()
+	}
+
+	os.Exit(code)
 }
 
 func radians(degrees float64) float64 {
